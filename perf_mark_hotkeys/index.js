bd3d74c951be440bac2244df357f6f8a02cae365
askeing <askeing@gmail.com>
2016-05-03 17:42:59 +0800

    update MPL 2.0 header

--- perf_mark_hotkeys/index.js
+++ perf_mark_hotkeys/index.js
@@ -1,3 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Created by Askeing on 2016/5/3.
+ */
+
 var self = require("sdk/self");
 var pageMod = require("sdk/page-mod");
 var sp = require("sdk/simple-prefs");
