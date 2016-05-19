d6ecec58298dce2f86733f07f567e401f9e87de2
askeing <askeing@gmail.com>
2016-05-19 14:10:14 +0800

    update perf_mark_hotkeys to 0.0.3

--- perf_mark_hotkeys/index.js
+++ perf_mark_hotkeys/index.js
@@ -20,6 +20,14 @@ var msg_pm7 = sp.prefs["pm7"];
 var msg_pm8 = sp.prefs["pm8"];
 var msg_pm9 = sp.prefs["pm9"];
 var msg_pm0 = sp.prefs["pm0"];
+var enable_pm3 = sp.prefs["enable_pm3"];
+var enable_pm4 = sp.prefs["enable_pm4"];
+var enable_pm5 = sp.prefs["enable_pm5"];
+var enable_pm6 = sp.prefs["enable_pm6"];
+var enable_pm7 = sp.prefs["enable_pm7"];
+var enable_pm8 = sp.prefs["enable_pm8"];
+var enable_pm9 = sp.prefs["enable_pm9"];
+var enable_pm0 = sp.prefs["enable_pm0"];
 
 // if prefs are changed, then update to the latest prefs
 function onPrefChange(prefName) {
@@ -32,9 +40,56 @@ function onPrefChange(prefName) {
     msg_pm9 = sp.prefs["pm9"];
     msg_pm0 = sp.prefs["pm0"];
 }
+function onPrefChange_enable_pm3() {
+    enable_pm3 = sp.prefs["enable_pm3"];
+    pm3_Hotkey = set_pm(enable_pm3, pm3_Hotkey, "control-shift-3", msg_pm3);
+}
+function onPrefChange_enable_pm4() {
+    enable_pm4 = sp.prefs["enable_pm4"];
+    pm4_Hotkey = set_pm(enable_pm4, pm4_Hotkey, "control-shift-4", msg_pm4);
+}
+function onPrefChange_enable_pm5() {
+    enable_pm5 = sp.prefs["enable_pm5"];
+    pm5_Hotkey = set_pm(enable_pm5, pm5_Hotkey, "control-shift-5", msg_pm5);
+}
+function onPrefChange_enable_pm6() {
+    enable_pm6 = sp.prefs["enable_pm6"];
+    pm6_Hotkey = set_pm(enable_pm6, pm6_Hotkey, "control-shift-6", msg_pm6);
+}
+function onPrefChange_enable_pm7() {
+    enable_pm7 = sp.prefs["enable_pm7"];
+    pm7_Hotkey = set_pm(enable_pm7, pm7_Hotkey, "control-shift-7", msg_pm7);
+}
+function onPrefChange_enable_pm8() {
+    enable_pm8 = sp.prefs["enable_pm8"];
+    pm8_Hotkey = set_pm(enable_pm8, pm8_Hotkey, "control-shift-8", msg_pm8);
+}
+function onPrefChange_enable_pm9() {
+    enable_pm9 = sp.prefs["enable_pm9"];
+    pm9_Hotkey = set_pm(enable_pm9, pm9_Hotkey, "control-shift-9", msg_pm9);
+}
+function onPrefChange_enable_pm0() {
+    enable_pm0 = sp.prefs["enable_pm0"];
+    pm0_Hotkey = set_pm(enable_pm0, pm0_Hotkey, "control-shift-0", msg_pm0);
+}
 
 // listen on prefs change
-sp.on("", onPrefChange);
+sp.on("pm3", onPrefChange);
+sp.on("pm4", onPrefChange);
+sp.on("pm5", onPrefChange);
+sp.on("pm6", onPrefChange);
+sp.on("pm7", onPrefChange);
+sp.on("pm8", onPrefChange);
+sp.on("pm9", onPrefChange);
+sp.on("pm0", onPrefChange);
+sp.on("enable_pm3", onPrefChange_enable_pm3);
+sp.on("enable_pm4", onPrefChange_enable_pm4);
+sp.on("enable_pm5", onPrefChange_enable_pm5);
+sp.on("enable_pm6", onPrefChange_enable_pm6);
+sp.on("enable_pm7", onPrefChange_enable_pm7);
+sp.on("enable_pm8", onPrefChange_enable_pm8);
+sp.on("enable_pm9", onPrefChange_enable_pm9);
+sp.on("enable_pm0", onPrefChange_enable_pm0);
 
 // load pageMode scripts
 var pm = pageMod.PageMod({
@@ -43,58 +98,32 @@ var pm = pageMod.PageMod({
     contentScriptFile: self.data.url("perf_mark_hotkeys.js"),
 });
 
-Hotkey({
-    combo: "control-shift-3",
-    onPress: function() {
-        pm.port.emit("addPerfMark", msg_pm3);
-    }
-});
+var pm3_Hotkey, pm4_Hotkey, pm5_Hotkey, pm6_Hotkey, pm7_Hotkey, pm8_Hotkey, pm9_Hotkey, pm0_Hotkey;
 
-Hotkey({
-    combo: "control-shift-4",
-    onPress: function() {
-        pm.port.emit("addPerfMark", msg_pm4);
+function set_pm(enable_pref, hotkey_handler, combo_key, msg_pref) {
+    if (enable_pref) {
+        if(typeof(hotkey_handler) != "undefined" || hotkey_handler) {
+            hotkey_handler.destroy();
+        }
+        hotkey_handler = Hotkey({
+            combo: combo_key,
+            onPress: function () {
+                pm.port.emit("addPerfMark", msg_pref);
+            }
+        });
+    } else {
+        if(typeof(hotkey_handler) != "undefined" || hotkey_handler) {
+            hotkey_handler.destroy();
+        }
     }
-});
+    return hotkey_handler;
+}
 
-Hotkey({
-    combo: "control-shift-5",
-    onPress: function() {
-        pm.port.emit("addPerfMark", msg_pm5);
-    }
-});
-
-Hotkey({
-    combo: "control-shift-6",
-    onPress: function() {
-        pm.port.emit("addPerfMark", msg_pm6);
-    }
-});
-
-Hotkey({
-    combo: "control-shift-7",
-    onPress: function() {
-        pm.port.emit("addPerfMark", msg_pm7);
-    }
-});
-
-Hotkey({
-    combo: "control-shift-8",
-    onPress: function() {
-        pm.port.emit("addPerfMark", msg_pm8);
-    }
-});
-
-Hotkey({
-    combo: "control-shift-9",
-    onPress: function() {
-        pm.port.emit("addPerfMark", msg_pm9);
-    }
-});
-
-Hotkey({
-    combo: "control-shift-0",
-    onPress: function() {
-        pm.port.emit("addPerfMark", msg_pm0);
-    }
-});
+pm3_Hotkey = set_pm(enable_pm3, pm3_Hotkey, "control-shift-3", msg_pm3);
+pm4_Hotkey = set_pm(enable_pm4, pm4_Hotkey, "control-shift-4", msg_pm4);
+pm5_Hotkey = set_pm(enable_pm5, pm5_Hotkey, "control-shift-5", msg_pm5);
+pm6_Hotkey = set_pm(enable_pm6, pm6_Hotkey, "control-shift-6", msg_pm6);
+pm7_Hotkey = set_pm(enable_pm7, pm7_Hotkey, "control-shift-7", msg_pm7);
+pm8_Hotkey = set_pm(enable_pm8, pm8_Hotkey, "control-shift-8", msg_pm8);
+pm9_Hotkey = set_pm(enable_pm9, pm9_Hotkey, "control-shift-9", msg_pm9);
+pm0_Hotkey = set_pm(enable_pm0, pm0_Hotkey, "control-shift-0", msg_pm0);
